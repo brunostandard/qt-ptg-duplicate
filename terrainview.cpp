9de4702917594467b420ee0e8ed4e3f093ad2a16
Tim Andersen <timandersen32@gmail.com>
2019-05-03 15:55:08 -0700

    Add .png textures to the TerrainView.

--- terrainview.cpp
+++ terrainview.cpp
@@ -17,36 +17,36 @@ TerrainView::TerrainView(QWidget *parent)
     layout->addWidget(view);
     this->setLayout(layout);
     // TEST ITEMS
-    data::DiamondSquare ds(513);
+    data::DiamondSquare ds(257);
     ds();
     std::vector<std::vector<int>> map = ds.getMap();
 
-    for(size_t y = 0; y < 513; y++)
+    for(size_t y = 0; y < 257; y++)
     {
-        for(size_t x = 0; x < 513; x++)
+        for(size_t x = 0; x < 257; x++)
         {
             size_t xSize = 4;
             size_t ySize = 4;
 
-            QPixmap p = QPixmap(xSize,ySize);
+            QPixmap p = QPixmap(xSize, ySize);
 
             //Fill
             int value = map[x][y];
 
-            if(value < 130)
-                p.fill(Qt::darkBlue);
+            if(value < 100)
+                p = QPixmap("C:\\Users\\Timnus32\\Documents\\pic10c-ptg\\textures\\deepOcean.png");
             else if(value < 150)
-                p.fill(Qt::blue);
-            else if(value < 200)
-                p.fill(Qt::darkGreen);
-            else if(value < 230)
-                p.fill(Qt::darkYellow);
+                p = QPixmap("C:\\Users\\Timnus32\\Documents\\pic10c-ptg\\textures\\ocean.png");
+            else if(value < 180)
+                p = QPixmap("C:\\Users\\Timnus32\\Documents\\pic10c-ptg\\textures\\coast.png");
+            else if(value < 225)
+                p = QPixmap("C:\\Users\\Timnus32\\Documents\\pic10c-ptg\\textures\\land.png");
             else if(value < 250)
-                p.fill(Qt::gray);
-            else if(value < 260)
-                p.fill(Qt::darkGray);
+                p = QPixmap("C:\\Users\\Timnus32\\Documents\\pic10c-ptg\\textures\\hill.png");
+            else if(value < 280)
+                p = QPixmap("C:\\Users\\Timnus32\\Documents\\pic10c-ptg\\textures\\mountain.png");
             else
-                p.fill(Qt::white);
+                p = QPixmap("C:\\Users\\Timnus32\\Documents\\pic10c-ptg\\textures\\peak.png");
 
             QGraphicsPixmapItem * pi = new QGraphicsPixmapItem(p);
             scene->addItem(pi);
